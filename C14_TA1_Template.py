04e0b8a141be1bd1aaeb535d1d7a28f3556a6da0
whjr2021 <91585088+whjr2021@users.noreply.github.com>
2021-11-26 12:18:54 +0530

    Add files via upload

---
+++ C14_TA1_Template.py
@@ -0,0 +1,55 @@
+# TA1a-Step 1: Create a class "Dog"
+
+    # TA1a-Step 2: Add the two properties to the 'Dog' class using variables.
+
+
+
+    # TA1c-Step 1: Initialise the instance variables through a constructor.
+    
+    
+    
+    
+    # TA1a-Step 3: Add the two methods "run()" and "bark()" inside the 'Dog' class.
+
+
+
+
+
+      
+# TA1a-Step 4: Create an instance or object of the 'Dog' class. Name them as 'tommy', 'scooby', 'dodo' 
+
+
+
+
+# TA1b-Step 1: Access the properties and methods of 'tommy'
+# 1. Print all properties of the 'tommy' object.
+
+
+# 2. Print the value returned by the 'run()' function of 'tommy'.
+
+# 3. Print the value returned by the 'bark()' function  of 'tommy'.
+print("-------------------")
+# TA1b-Step 2: Access the properties and methods of 'dodo'
+# 1. Print all properties of the 'dodo' object.
+
+
+# 2. Print the value returned by the 'run()' function  of 'dodo'.
+
+# 3. Print the value returned by the 'bark()' function  of 'dodo'.
+
+# NOTE: Before coding for TA1c- Step 2: erase everything from TA1b-Step 1 to TA1b-Step 2 i.e. lines 24 to 39.
+#       and define the 'dog1' object by passing instance variable values.
+# TA1c- Step 2: Create an object 'dog1' of class "Dog" and enter the values as 'Tommy', 'Labrador' inside the bracket.
+
+# 1. Print the name of 'dog1' object function.
+
+# 2. Print the breed of 'dog1' object function.
+
+# 3. Print the owner and tail property values.
+
+
+# 4. Print the value returned by the 'run()' function of 'tommy'.
+
+# 5. Print the value returned by the 'bark()' function  of 'tommy'.
+
+  
